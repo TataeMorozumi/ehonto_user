81146a2ee5db268aa1828dc35551abe5d899d13c
TataeMorozumi <u.tatae@gmail.com>
2025-03-05 13:21:08 +0900

    それぞれ遷移先のページを作成

---
+++ ehonto/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('favorite/', views.favorite, name='favorite'),
+    path('review/', views.review, name='review'),
+    path('more_read/', views.more_read, name='more_read'),
+    path('settings/', views.settings, name='settings'),
+    path('family_invite/', views.family_invite, name='family_invite'),
+]
--- ehonto/app/views.py
+++ ehonto/app/views.py
@@ -9,12 +9,41 @@ class PortfolioView(View):
     
 class SignupView(View):
     def get(self, request):
-        return render(request, "signup.html")  
-
+        form = SignupForm()
+        return render(request, "signup.html", {"form": form})  
+    
+    def post(self, request):
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.first_name = form.cleaned_data["first_name"]
+            user.last_name = form.cleaned_data["last_name"]
+            user.email = form.cleaned_data["email"]
+            user.save()
+            login(request, user)  # 自動ログイン
+            return redirect("home")  # ホーム画面へリダイレクト
+        return render(request, "accounts/signup.html", {"form": form})
+    
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html")  
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "home.html")            
+        return render(request, "home.html")    
+
+def favorite(request):
+    return render(request, 'favorite.html')
+
+def review(request):
+    return render(request, 'review.html')
+
+def more_read(request):
+    return render(request, 'more_read.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def family_invite(request):
+    return render(request, 'family_invite.html')
+        
