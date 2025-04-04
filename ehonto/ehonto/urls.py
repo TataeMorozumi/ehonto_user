0a606e3a6a97361a4276df4ed61bc7491fcf7e38
TataeMorozumi <u.tatae@gmail.com>
2025-04-04 23:38:41 +0900

    Fix: signup修正

--- ehonto/ehonto/urls.py
+++ ehonto/ehonto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import PortfolioView, SignupView, LoginView, HomeView, settings_view,signup_view
+from app.views import PortfolioView, LoginView, HomeView, settings_view,signup_view
 from django.contrib.auth import views as auth_views
 
 
