143378fa577d8455d1b6b3a00f47aa3e2ca54ea0
Tatae Morozumi <tatae@example.com>
2025-04-04 16:41:31 +0000

    Fix urls.py and update .gitignore from PythonAnywhere

--- ehonto/urls.py
+++ ehonto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import PortfolioView, SignupView, LoginView, HomeView, settings_view
+from app.views import PortfolioView, LoginView, HomeView, settings_view
 from django.contrib.auth import views as auth_views
 from app.views import signup_view
 urlpatterns = [
