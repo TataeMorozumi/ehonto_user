73acb129f431b879c0b2ff6278ed5d455ea8f5c8
TataeMorozumi <u.tatae@gmail.com>
2025-04-05 15:29:52 +0900

    Signup

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views  # ✅ views モジュール全体をインポート
 from .views import (
-    LoginView, HomeView, book_detail, delete_book, add_book,
+    SignupView, LoginView, HomeView, book_detail, delete_book, add_book,
     child_edit, child_add, child_bookshelf,
     favorite, review, more_read,
     settings_view, family_invite,
@@ -28,6 +28,7 @@ urlpatterns = [
 
     # ✅ 認証関連
     path("signup/", signup_view, name="signup"),
+    path("signup/", SignupView.as_view(), name="signup"),
 
     # ✅ ホーム & 絵本登録
     path('', home_view, name='home'),  
