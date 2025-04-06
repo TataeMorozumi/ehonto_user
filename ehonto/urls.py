71539c58a2e54d60242f75304b6d611d7b43faa4
TataeMorozumi <u.tatae@gmail.com>
2025-04-07 01:13:03 +0900

    LoginView削除

--- ehonto/urls.py
+++ ehonto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import PortfolioView, LoginView, HomeView, settings_view
+from app.views import PortfolioView, HomeView, settings_view
 from django.contrib.auth import views as auth_views
 from app.views import SignupView
 urlpatterns = [
