5b2447c5d9aafb28c623a01f15a5035b98d47d0b
TataeMorozumi <u.tatae@gmail.com>
2025-04-08 20:50:54 +0900

    app.apps.AppCustomConfig

--- app/apps.py
+++ app/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class AppCustomConfig(AppConfig):  # ✅ クラス名をユニークに
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-   
