d23941dfe8d40c6576ef1421eb74b741be57e438
TataeMorozumi <u.tatae@gmail.com>
2025-04-08 19:28:40 +0900

    Fix wsgi.py for correct application path

--- ehonto/wsgi.py
+++ ehonto/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ehonto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ehonto.ehonto.wsgi.application')
 
 application = get_wsgi_application()
