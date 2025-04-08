38ae666f9626b54ca9baf4ae7dc83c788382d106
Tatae Morozumi <tatae@example.com>
2025-04-08 13:14:20 +0000

    Merge conflict resolved in apps.py

--- ehonto/app/apps.py
+++ ehonto/app/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class EhontoAppConfig(AppConfig): # ✅ クラス名をユニークに
+class EhontoAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
