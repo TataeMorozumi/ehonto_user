b8088bec791e84c8670d9cca88f746b0d48aa7cf
TataeMorozumi <u.tatae@gmail.com>
2025-04-14 12:46:47 +0900

    Fix INSTALLED_APPS to use EhontoAppConfig

--- ehonto/ehonto/settings.py
+++ ehonto/ehonto/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ehonto_user.app.apps.AppConfig',
+    'app.apps.EhontoAppConfig',
 ]
 
 MIDDLEWARE = [
